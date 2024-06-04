c2c6a5e66146e9025de082560a9624338bca4e67
Saulo Román Santillán Nava <romansantillan1998@outlook.com>
2024-06-04 12:54:22 -0600

    Removing log

--- src/config/api/abrasaApi.js
+++ src/config/api/abrasaApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getEnvVariables } from '../../helpers';
 
 const { VITE_API_URL_PROD } = getEnvVariables();
-console.log(VITE_API_URL_PROD);
+
 const abrasaApi = axios.create({
   baseURL: VITE_API_URL_PROD,
 });
